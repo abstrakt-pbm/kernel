2bb1e7ade17e499516e2e6f1fe778c6ad23ad401
Abstrakt <abstraktpbm@gmail.com>
2025-11-22 13:50:03 +0300

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Experimental kernel
+
+## Building 
+cmake --build --preset=lin_gcc
+mv ./bin/kernel.elf ./bin/iso/boot/kernel.elf
+grub-mkrescue -o ./bin/kernel.iso ./bin/iso
+
+## Start
+qemu-system-x86_64 \
+  -bios /usr/share/OVMF/x64/OVMF_CODE.fd \
+  -drive format=raw,file=./bin/kernel.iso \
+  -m 4G \
+  -vga virtio \
+  -S \
+  -gdb tcp::1234
+
+## Screenshots
+![Terminal HelloWorld](screenshots/terminal_helloworld.png)
---
+++ kernel-hlvl/subsystems/fs/fsdriver/fsdriver.cpp
@@ -0,0 +1,50 @@
+#include <fs/fsnode.hpp>
+
+namespace FS {
+
+FSNode::FSNode(String&& name,
+			   FSNodeType type)
+: name_(name),
+type_(type) {}
+
+
+bool FSNode::mkchild(unique_ptr<FSNode> child) {
+	if (this->type_ == FSNodeType::DIR) {
+		childs_.push_back(move(child));
+		return true;
+	}
+	return false;
+}
+
+size_t FSNode::child_count() const {
+	return childs_.size();
+}
+
+const String& FSNode::name() const {
+	return name_;
+}
+FSNodeType FSNode::type() const {
+	return type_;
+}
+bool FSNode::isOpen() const {
+	return is_open_;
+}
+
+Dirrectory::Dirrectory(String&& name)
+: FSNode(move(name), FSNodeType::DIR) {}
+
+File::File(String&& name,
+		String&& payload) 
+: FSNode(move(name), FSNodeType::FILE),
+payload_(move(payload)){}
+
+ListIterator<unique_ptr<FSNode>> Dirrectory::begin() {
+	return childs_.begin();
+}
+
+ListIterator<unique_ptr<FSNode>> Dirrectory::end() {
+	return childs_.end();
+}
+
+} // namespace FS
+
